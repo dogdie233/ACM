8bb3332e600d71a35e7a3e9014357c8007c3cbc1
dogdie233 <dogdie233@163.com>
2025-01-26 14:55:08 +0800

    [Nowcoder][95335][AC][CPP] F

--- nowcoder/95335/F.cpp
+++ nowcoder/95335/F.cpp
@@ -14,7 +14,30 @@ int main()
     while (t--)
     {
         cin >> n >> a >> b >> c;
-        sum = a + b + c;
-        cout << (sum % 2 == 0 && sum / 2 == n ? "Yes" : "No") << endl;
+
+        int S = a + b + c - n;
+        if (S < 0) {
+            cout << "No" << endl;
+            continue;
+        }
+
+        bool possible = false;
+        for (int x1 = 0; x1 <= min({a, c, S}); x1++)
+        {
+            for (int x3 = 0; x3 <= min(a - x1, b); x3++)
+            {
+                int x5 = S - x1 - x3;
+                if (x5 < 0 || x5 > c - x1 || x5 > b - x3)
+                    continue;
+                if (x1 + x3 <= a && x3 + x5 <= b && x1 + x5 <= c)
+                {
+                    possible = true;
+                    break;
+                }
+            }
+            if (possible)
+                break;
+        }
+        cout << (possible ? "Yes" : "No") << endl;
     }
 }

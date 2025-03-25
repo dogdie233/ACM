275211092499884b1bc0522cb90ba044d8cd2da5
dogdie233 <dogdie233@163.com>
2025-03-25 14:03:59 +0800

    [Tianti][2025][WA][CPP] 13

---
+++ tianti/13.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    #ifdef ONLINE_JUDGE
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    #endif
+    uint64_t N, owo = 0, sum = 0;
+    cin >> N;
+    for (int n = 1; n <= N; n++) {
+        owo += n;
+        sum += owo;
+    }
+    cout << sum;
+    return 0;
+}

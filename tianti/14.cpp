1a61d92c70c311f6946849667fbf7591efc4df5b
dogdie233 <dogdie233@163.com>
2025-03-25 14:06:14 +0800

    [Tianti][2025][AC][CPP] 14

---
+++ tianti/14.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    #ifdef ONLINE_JUDGE
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    #endif
+    cout << "This is a simple problem.";
+    return 0;
+}

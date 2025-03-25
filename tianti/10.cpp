431650d14b7f958ff18fb5f1c6b1db2cf7b71c0c
dogdie233 <dogdie233@163.com>
2025-03-25 13:44:21 +0800

    [Tianti][2025][AC][CPP] 10

---
+++ tianti/10.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    #ifdef ONLINE_JUDGE
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    #endif
+    int a, b, c;
+    cin >> a >> b >> c;
+    if (a > b) swap(a, b);
+    if (b > c) swap(b, c);
+    if (a > b) swap(a, b);
+    cout << a << "->" << b << "->" << c << endl;
+    return 0;
+}

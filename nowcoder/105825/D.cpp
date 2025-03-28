feb58bb2b2a20cf226f0a18f157195983c1ce291
dogdie233 <dogdie233@163.com>
2025-03-28 20:09:56 +0800

    [NowCoder][105825][TLE][CPP] D

--- nowcoder/105825/D.cpp
+++ nowcoder/105825/D.cpp
@@ -34,34 +34,33 @@ int main() {
         cin >> *it;
     }
     sort(arr.begin(), arr.end());
-    auto notZeroBegin = upper_bound(arr.begin(), arr.end(), 0);
-    if (notZeroBegin == arr.end()) {
+    if (all_of(arr.begin(), arr.end(), [&arr](num_t x) { return x == *arr.begin(); })) {  // 全是0
         return cout << 0 << endl, 0;
-    } else if (notZeroBegin == arr.begin()) {
+    }
+
+    auto notZeroBegin = upper_bound(arr.begin(), arr.end(), 0);
+    if (notZeroBegin == arr.end()) {  // 全是0
+        return cout << 0 << endl, 0;
+    } else if (notZeroBegin == arr.begin()) {  // 没有0
         return cout << -1 << endl, 0;
     }
     
     while (notZeroBegin != arr.end()) {
         auto mexValue = mex(notZeroBegin, arr.end(), 1);
-        auto divCount = (num_t)0;
+        auto opCount = (num_t)0;
         
         if (mexValue == 1) {
-            if (*notZeroBegin == 1) {
-                divCount = 1;
-            } else {
-                divCount = *notZeroBegin - 1;
-            }
+            opCount = max(1ll, *notZeroBegin - 1);
         } else {
-            divCount = 1;
+            opCount = 1;
         }
 
-        ans += divCount;
-        auto reduceCount = divCount * mexValue;
+        ans += opCount;
+        auto reduceCount = opCount * mexValue;
         for (auto it = notZeroBegin; it != arr.end(); ++it) {
             *it = max(0ll, *it - reduceCount);
         }
         notZeroBegin = upper_bound(notZeroBegin, arr.end(), 0);
     }
-    cout << ans;
-    return 0;
+    return cout << ans << endl, 0;
 }

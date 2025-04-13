40073bc9156ffdb14ab7f1ad836a41bf63a485df
dogdie233 <dogdie233@163.com>
2025-04-13 20:12:39 +0800

    [NowCoder][107000][AC][CPP] C

---
+++ nowcoder/107000/C.cpp
@@ -0,0 +1,30 @@
+// Nowcoder Compiler: C++（clang++18）
+#include <iostream>
+#include <stdint.h>
+
+using namespace std;
+
+const int64_t MOD = 1e9 + 7;
+
+int64_t pow(int64_t b, int64_t e, int64_t m) {
+    int64_t ans = 1;
+    while (e) {
+        if (e & 1) {
+            ans = (ans * b) % m;
+        }
+        b = (b * b) % m;
+        e >>= 1;
+    }
+    return ans;
+}
+
+int main() {
+    #ifdef ONLINE_JUDGE
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    #endif
+    int64_t n, k, qwq;
+    cin >> n >> k;
+    cout << pow(2, n - k + 1, MOD) << endl;
+    return 0;
+}
